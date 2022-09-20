5af54623f5f1c567d0404643ef83fd328dc5551b
InsomniaTSO <InsomniaTSO@yandex.ru>
2022-09-20 10:40:43 +0300

    settings fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default=None)
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
