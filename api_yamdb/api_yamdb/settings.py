25d9fed65cee13ebd8dcf5a32768f71df30ed9bc
InsomniaTSO <InsomniaTSO@yandex.ru>
2022-09-22 21:33:51 +0300

    1st review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,9 +1,13 @@
 import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', default=None)
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret_django_key')
 
 DEBUG = False
 
